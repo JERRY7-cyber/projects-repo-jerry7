b7edfae45291963d371bddb5eee1f28d1ec7cfb3
J E R R Y 7 <jerryxjerry111@gmail.com>
2025-09-25 16:24:39 +0500

    Update

---
+++ Project_04/JS/script.js
@@ -0,0 +1,14 @@
+const tabLinks = document.getElementsByClassName("tabLinks");
+const tabContents = document.getElementsByClassName("tabContents");
+
+
+function openTab(tabName){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove('activeLink');
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove('activeTab');
+    }
+    event.currentTarget.classList.add('activeLink');
+    document.getElementById(tabName).classList.add('activeTab');
+}
